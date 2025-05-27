d3723a6127948d75a74e46c35dac143d55ef520c
korajohnston <152660234+korajohnston@users.noreply.github.com>
2025-05-27 13:51:24 -0700

    hi
    hi

--- Assets/Scripts/CollectCoins.cs
+++ Assets/Scripts/CollectCoins.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI; // Ensure you have UnityEngine.UI imported for UI text handling
 using System.Collections;
-using TMPro;
+using TMPro; // Ensure you have TextMeshPro imported for UI text handling
 
 public class CollectCoins : MonoBehaviour
 {
     public int coins = 0; // Variable to store the number of coins collected
+    public int speedPotion = 0; // Variable to store the number of speed potions collected
+    public int healthPotion = 0; // Variable to store the number of health potions collected
     public TextMeshProUGUI coinText; // Reference to the UI text element
-    public GameObject coin; // Reference to the coin prefab
+    public TextMeshProUGUI healthText; // Reference to the UI text element for health
+    public TextMeshProUGUI speedPotionText; // Reference to the UI text element for speed
+    public TextMeshProUGUI healthPotionText; // Reference to the UI text element for score
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +23,18 @@ public class CollectCoins : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (coinText == null)
+        {
+            coinText = GameObject.Find("CoinCounter").GetComponent<TextMeshProUGUI>();
+        }
+        if (healthText == null)
+        {
+            healthText = GameObject.Find("HealthCounter").GetComponent<TextMeshProUGUI>();
+        }
+        if (speedPotionText == null)
+        {
+            speedPotionText = GameObject.Find("SpeedCounter").GetComponent<TextMeshProUGUI>();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -43,13 +61,34 @@ public class CollectCoins : MonoBehaviour
         if (other.gameObject.CompareTag("Health"))
         {
             // Logic to give the player health
-            Debug.Log("Health collected!");
+            print("Health collected!");
+            healthPotion ++; // Increment health potion count
+            print(healthPotion);
+
+            if (healthPotionText != null)
+            {
+                healthPotionText.text = "Health Potions: " + healthPotion;
+            }
+            else
+            {
+                Debug.LogWarning("healthPotionText is not assigned in the Inspector!");
+            }
+
             Destroy(other.gameObject); // Destroy the collected health item
         }
         if (other.gameObject.CompareTag("Speed"))
         {
-            // Logic to give the player speed boost
-            Debug.Log("Speed collected!");
+            print("Speed collected!");
+            speedPotion = speedPotion + 1; // Increment speed potion count
+
+            if (speedPotionText != null)
+            {
+                speedPotionText.text = "Speed Potions: " + speedPotion;
+            }
+            else
+            {
+                Debug.LogWarning("speedPotionText is not assigned in the Inspector!");
+            }
             Destroy(other.gameObject); // Destroy the collected speed item
         }
     }
